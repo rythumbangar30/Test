5fdde3cc4f4c0b16461623159d4679cba65ebc91
Rythum Bangar <shobhit.dubey@liseinfotech.com>
2023-12-13 18:54:09 +0530

    Loop add

---
+++ src/main/java/org/example/Loop.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Loop {
+    public static void main(String[] args) {
+        int i=0,j=0;
+        for(i=0;i<5;i++)
+        {
+            for(j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
