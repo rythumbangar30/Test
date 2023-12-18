a465471c77f1a765710c2f5d75442202bb8fcf1b
rythumbangar30 <153621109+rythumbangar30@users.noreply.github.com>
2023-12-18 15:47:58 +0530

    Update Map1.java

---
+++ Java/Map1.java
@@ -0,0 +1,23 @@
+//To convert the map into set and print key and value separate
+import java.util.*;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+public class Map1 {
+    public static void main(String[] args) {
+        Map m1=new HashMap();
+        m1.put(1,"Mon");
+        m1.put(2,"Tue");
+        m1.put(3,"Wed");
+        m1.put(4,"Thru");
+        System.out.print(m1);
+
+        Set set=m1.entrySet();
+        Iterator i1=set.iterator();
+        while(i1.hasNext()){
+            Map.Entry entry=(Map.Entry)i1.next();
+            
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
+    }
+    
+}
